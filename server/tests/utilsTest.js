7da279e73eba02d7939286914d80c94797f76831
BOAKGP <mmm2000c4@gmail.com>
2017-01-08 14:38:04 +0100

    fix

--- server/tests/utilsTest.js
+++ server/tests/utilsTest.js
@@ -4,25 +4,6 @@ const expect = chai.expect;
 const UniqueKeyGenerator = require( './../lib/Utils/UniqueKeyGenerator' );
 const UniqueNameStorage = require( './../lib/Utils/UniqueNameStorage' );
 const SearchEngine = require( './../lib/Utils/SearchEngine' );
-describe( 'Numbers', function()
-{
-	it( 'isUInt32() should return false if number is less than 0', function()
-	{
-		expect( Numbers.isUInt32( -1 ) ).to.equal( false );
-	} );
-	it( 'isUInt32() should return false if number is bigger or equal 2^32', function()
-	{
-		expect( Numbers.isUInt32( Math.pow( 2, 32 ) ) ).to.equal( false );
-	} );
-	it( 'isUInt32() should return false if parameter is not number', function()
-	{
-		expect( Numbers.isUInt32( 'not uint32' ) ).to.equal( false );
-	} );
-	it( 'isUInt32() should return true for correct uint32', function()
-	{
-		expect( Numbers.isUInt32( 1337 ) ).to.equal( true );
-	} );
-} );
 describe( 'UniqueKeyGenerator', function()
 {
 	it( 'generateKey() should return buffer', function()
