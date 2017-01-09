61fe908451e71ba92099281cf09c2a735e5b8835
BOAKGP <mmm2000c4@gmail.com>
2017-01-09 20:25:58 +0100

    fix

--- server/lib/Network/Room.js
+++ server/lib/Network/Room.js
@@ -218,7 +218,7 @@ class Room {
 			} );
 		}
 		let ready = Buffer.alloc( 1 );
-		ready.writeUInt8( this.getReadyPlayersAmount, 0 );
+		ready.writeUInt8( this.getReadyPlayersAmount(), 0 );
 		this.playersList = {
 			ready,
 			count,
