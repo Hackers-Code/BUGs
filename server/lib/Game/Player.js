5daa9540594a9d928391bd643fcd3e7732ea76f3
BOAKGP <mmm2000c4@gmail.com>
2017-01-24 22:29:51 +0100

    Fix

--- server/lib/Game/Player.js
+++ server/lib/Game/Player.js
@@ -3,7 +3,7 @@ class Player {
 	constructor( client, playerID )
 	{
 		client.player = this;
-		this.response = client.response;
+		this.send = client.send;
 		this.id = client.id;
 		this.playerID = playerID;
 		this.confirmed = false;
