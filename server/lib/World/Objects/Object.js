e700281bd3144f7696c7b59d54eb65cece80933a
BOAKGP <mmm2000c4@gmail.com>
2017-04-08 23:58:52 +0200

    Object class modified

--- server/lib/World/Objects/Object.js
+++ server/lib/World/Objects/Object.js
@@ -1,18 +1,14 @@
+const SAT = require( 'sat' );
 class Object {
 	constructor( data )
 	{
-		this.x = typeof data.x !== 'undefined' ?
-		         data.x.readInt32BE( 0 ) :
-		         0;
-		this.y = typeof data.y !== 'undefined' ?
-		         data.y.readInt32BE( 0 ) :
-		         0;
-		this.width = typeof data.width !== 'undefined' ?
-		             data.width.readInt32BE( 0 ) :
-		             0;
-		this.height = typeof data.height !== 'undefined' ?
-		              data.height.readInt32BE( 0 ) :
-		              0;
+		let x = typeof data.x !== 'undefined' ? data.x.readInt32BE( 0 ) : 0;
+		let y = typeof data.y !== 'undefined' ? data.y.readInt32BE( 0 ) : 0;
+		let width = typeof data.width !== 'undefined' ? data.width.readInt32BE( 0 ) : 0;
+		let height = typeof data.height !== 'undefined' ? data.height.readInt32BE( 0 ) : 0;
+		let corner = new SAT.Vector( x, y );
+		let box = new SAT.Box( corner, width, height );
+		this.hitbox = box.toPolygon();
 	}
 }
 module.exports = Object;
