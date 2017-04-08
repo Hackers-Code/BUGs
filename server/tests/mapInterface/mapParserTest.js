ad4e25129510f275a5665742db546f83e880249f
BOAKGP <mmm2000c4@gmail.com>
2017-04-08 21:47:40 +0200

    Unit tests fixed

--- server/tests/mapInterface/mapParserTest.js
+++ server/tests/mapInterface/mapParserTest.js
@@ -13,17 +13,18 @@ describe( 'MapParser', function()
 		} );
 		it( 'should throw exception if map contains unknown block', function()
 		{
-			expect( MapParser.parse.bind( MapParser, Buffer.from( '000000000000000007', 'hex' ) ) ).to
+			expect( MapParser.parse.bind( MapParser, Buffer.from( '0000000000000000000000000000000007', 'hex' ) ) ).to
 			.throw( 'Map file broken' );
 		} );
 		it( 'should throw exception if map contains broken block', function()
 		{
-			expect( MapParser.parse.bind( MapParser, Buffer.from( '000000000000000001', 'hex' ) ) ).to
+			expect( MapParser.parse.bind( MapParser, Buffer.from( '0000000000000000000000000000000001', 'hex' ) ) ).to
 			.throw( 'Unexpected end of file' );
 		} );
 		it( 'should return object if map is correct', function()
 		{
-			expect( MapParser.parse( Buffer.from( '0000000000000000010000000000000000', 'hex' ) ) ).to.be
+			expect( MapParser.parse( Buffer.from( '00000000000000000000000000000000010000000000000000', 'hex' ) ) ).to
+			.be
 			.an( 'object' );
 		} );
 	} );
