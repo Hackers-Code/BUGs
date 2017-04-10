6ca8bbd9c03c876f1cf060f73e27224bb68605e1
BOAKGP <mmm2000c4@gmail.com>
2017-04-10 18:30:53 +0200

    GameServe class prototype

---
+++ server/lib2.0/GameServer/GameServer.js
@@ -0,0 +1,49 @@
+'use strict';
+const EncodePacket = require( './Protocol/PacketEncoder' );
+const ClientsStorage = require( './Clients/ClientsStorage' );
+const startTCPSocket = require( './tcpSocket' );
+const startUDPSocket = require( './udpSocket' );
+const Logger = require( './Logger' );
+class GameServer {
+	constructor( tcpPort, udpPort, maxClients )
+	{
+		this.logger = new Logger();
+		this.clientsStorage = new ClientsStorage();
+		this.maxClients = maxClients;
+		startTCPSocket( tcpPort, this.handleTCPConnection.bind( this ), this.handleError.bind( this ),
+			this.handleListening.bind( this ) );
+		startUDPSocket( udpPort, this.handleUDPPacket.bind( this ), this.handleError.bind( this ),
+			this.handleListening.bind( this ) );
+	}
+
+	handleTCPConnection( address, socketWrite )
+	{
+		this.logger.log( `Connection from ${address.remoteAddress}:${address.remotePort}` );
+		if( this.clientsStorage.getClientsCount() === this.maxClients )
+		{
+			socketWrite( EncodePacket( {
+				opcode : 0,
+				message : 'No free slots on server'
+			} ) );
+			return false;
+		}
+		return this.clientsStorage.addClient( socketWrite );
+	}
+
+	handleUDPPacket( packet, rinfo, socketSend )
+	{
+
+	}
+
+	handleError( error )
+	{
+		this.logger.error( error.message );
+	}
+
+	handleListening( type, address )
+	{
+		this.logger.log( `${type} socket listening on ${address.address}:${address.port}` );
+	}
+
+}
+module.exports = GameServer;
