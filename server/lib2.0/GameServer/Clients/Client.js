63a72ae49d8aab855fe8c77fa505b45f220428eb
BOAKGP <mmm2000c4@gmail.com>
2017-04-10 20:59:18 +0200

    Added basic network support for client (names and ids)

--- server/lib2.0/GameServer/Clients/Client.js
+++ server/lib2.0/GameServer/Clients/Client.js
@@ -1,8 +1,97 @@
 'use strict';
+const StreamParser = require( './StreamParser' );
+const EncodePacket = require( '../Protocol/PacketEncoder' );
+const DecodePacket = require( '../Protocol/PacketDecoder' );
 class Client {
-	constructor( socketWrite )
+	constructor( socketWrite, server, clientsStorage )
 	{
 		this.tcpSocketWrite = socketWrite;
+		this.server = server;
+		this.uniqueNameStorage = clientsStorage.getUniqueNameStorage();
+		this.streamParser = new StreamParser();
+		this.name = this.uniqueNameStorage.getDefault();
+	}
+
+	setId( id )
+	{
+		this.id = id;
+	}
+
+	getCallbacks()
+	{
+		return {
+			onData : this.handleData.bind( this ),
+			onClose : this.disconnect.bind( this ),
+			onError : null
+		};
+	}
+
+	handleData( data )
+	{
+		if( typeof data !== 'undefined' )
+		{
+			this.streamParser.appendData( data );
+		}
+		try
+		{
+			let buffer = this.streamParser.getBuffer();
+			let decoded = DecodePacket( buffer );
+			console.log( decoded );
+			if( decoded === false )
+			{
+				return;
+			}
+			this.streamParser.freeBufferToOffset( decoded.offset );
+			//this.respond( decoded.instruction, decoded.object );
+			this.handleData();
+		}
+		catch( e )
+		{
+			let msg;
+			let opcode;
+			if( e.message === 0xe0 )
+			{
+				msg = 'Unknown instruction';
+				opcode = 0xe0;
+			}
+			else
+			{
+				msg = 'Parsing request error';
+				opcode = 0xe2;
+			}
+			this.send( {
+				opcode : opcode,
+				error : msg
+			} );
+			this.streamParser.clearBuffer();
+		}
+	}
+
+	send( data, type = 'TCP' )
+	{
+		let encodedPacket = EncodePacket( data );
+		if( encodedPacket === false )
+		{
+			this.server.sendServerErrorMessage( this.tcpSocketWrite );
+			return;
+		}
+		if( type === 'TCP' )
+		{
+			this.tcpSocketWrite( encodedPacket );
+		}
+		else
+		{
+			//this.udpSend( encoded, this.rinfo.port, this.rinfo.address );
+		}
+	}
+
+	disconnect()
+	{
+		/*if( this.keepAliveUDP !== null )
+		 {
+		 clearInterval( this.keepAliveUDP );
+		 }
+		 this.clientsStorage.removeClient( this.id );*/
 	}
 }
 
--- server/lib2.0/GameServer/Clients/ClientsStorage.js
+++ server/lib2.0/GameServer/Clients/ClientsStorage.js
@@ -1,10 +1,19 @@
 'use strict';
 const Client = require( './Client' );
+const UniqueKeyGenerator = require( '../Helpers/UniqueKeyGenerator' );
+const UniqueNameStorage = require( '../Helpers/UniqueNameStorage' );
 class ClientsStorage {
 
 	constructor()
 	{
 		this.clients = [];
+		this.uniqueKeyGenerator = new UniqueKeyGenerator( 4 );
+		this.uniqueNameStorage = new UniqueNameStorage( 20, 'Anonymous' );
+	}
+
+	getUniqueNameStorage()
+	{
+		return this.uniqueNameStorage;
 	}
 
 	getClientsCount()
@@ -12,9 +21,12 @@ class ClientsStorage {
 		return this.clients.length;
 	}
 
-	addClient( socketWrite )
+	addClient( socketWrite, server )
 	{
-		this.clients.push( new Client( socketWrite ) );
+		let index = this.clients.push( new Client( socketWrite, server, this ) ) - 1;
+		let id = this.uniqueKeyGenerator.generateKey();
+		this.clients[ index ].setId( id );
+		return this.clients[ index ].getCallbacks();
 	}
 }
 
