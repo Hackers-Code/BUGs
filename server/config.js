00b10fae5f63eb760e5b9d49c270004ac902e40b
BOAKGP <mmm2000c4@gmail.com>
2017-04-10 16:44:55 +0200

    Deleted config file

--- server/config.js
+++
@@ -1,9 +0,0 @@
-const fs = require( 'fs' );
-module.exports = {
-	maxClients : 4,
-	logFile : fs.createWriteStream( process.cwd() + '/logs/log.txt' ),
-	errorFile : fs.createWriteStream( process.cwd() + '/logs/error.txt' ),
-	tickrate : 16,
-	TCP_port : 31337,
-	UDP_port : 31337
-};
