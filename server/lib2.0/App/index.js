0b5ad5b3e35a9a2ff8ebc53cbe4a1400f61dc67f
BOAKGP <mmm2000c4@gmail.com>
2017-04-13 17:07:13 +0200

    App now delegates new connections to PlayersStorage class

--- server/lib2.0/App/index.js
+++ server/lib2.0/App/index.js
@@ -1,9 +1,11 @@
 'use strict';
 const GameServer = require( '../GameServer' );
+const PlayersStorage = require( '../Players/PlayersStorage' );
 const ResourcesDownloader = require( '../ResourcesDownloader' );
 class App {
 	constructor()
 	{
+		this.playersStorage = new PlayersStorage();
 		this.resourcesDownloader = new ResourcesDownloader();
 		this.resourcesDownloader.download( this.startServer.bind( this ) );
 	}
@@ -11,6 +13,7 @@ class App {
 	startServer()
 	{
 		this.gameServer = GameServer( { maxClients : 4 } );
+		this.gameServer.on( 'connection', this.playersStorage.addPlayer.bind( this.playersStorage ) );
 	}
 }
 module.exports = App;
