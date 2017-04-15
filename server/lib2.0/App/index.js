0313c936caec48287d191673a373e40d83b5f3d4
BOAKGP <mmm2000c4@gmail.com>
2017-04-15 19:18:57 +0200

    Added logger,weapons interface and maps interface

--- server/lib2.0/App/index.js
+++ server/lib2.0/App/index.js
@@ -1,19 +1,46 @@
 'use strict';
+const Logger = require( './Logger' );
 const GameServer = require( '../GameServer' );
 const PlayersStorage = require( '../Players/PlayersStorage' );
 const ResourcesDownloader = require( '../ResourcesDownloader' );
+const MapInterface = require( '../MapInterface' );
+const WeaponsInterface = require( '../WeaponsInterface' );
 class App {
 	constructor()
 	{
+		this.logger = new Logger();
+		this.mapsList = [];
 		this.playersStorage = new PlayersStorage();
 		this.resourcesDownloader = new ResourcesDownloader();
 		this.resourcesDownloader.download( this.startServer.bind( this ) );
+		MapInterface.loadAndParseMapsList( this.loadMapsList.bind( this ) );
+		this.weaponsInterface = new WeaponsInterface();
+		this.weaponsInterface.loadWeaponsListFromFile( this.loadWeaponsList.bind( this ) );
 	}
 
 	startServer()
 	{
 		this.gameServer = GameServer( { maxClients : 4 } );
 		this.gameServer.on( 'connection', this.playersStorage.addPlayer.bind( this.playersStorage ) );
+	}
+
+	loadMapsList( error, data )
+	{
+		if( error )
+		{
+			console.log( 'Maps list does not exist' );
+			process.exit( 1 );
+		}
+		this.mapsList = data;
+	}
+
+	loadWeaponsList( error )
+	{
+		if( error )
+		{
+			console.log( 'Weapons list does not exist' );
+			process.exit( 1 );
+		}
 	}
 }
 module.exports = App;
