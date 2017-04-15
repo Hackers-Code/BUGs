9d297238af3ec5a5483ee62ff36035143c47e5b8
BOAKGP <mmm2000c4@gmail.com>
2017-04-15 23:41:38 +0200

    Added collection class

---
+++ server/lib/Collections/index.js
@@ -0,0 +1,41 @@
+class Collection {
+	constructor()
+	{
+		this.items = [];
+	}
+}
+
+class UniqueKeyCollection extends Collection {
+	find( key )
+	{
+		if( Buffer.isBuffer( key ) === false )
+		{
+			return -1;
+		}
+		for( let i = 0 ; i < this.items.length ; i++ )
+		{
+			if( id.compare( this.items[ i ].getUniqueKey() ) === 0 )
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+}
+
+class NumericIdCollection extends Collection {
+	find( id )
+	{
+		for( let i = 0 ; i < this.items.length ; i++ )
+		{
+			if( id === this.items[ i ].id )
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+}
+
+module.exports.UniqueKeyCollection = UniqueKeyCollection;
+module.exports.NumericIdCollection = NumericIdCollection;
