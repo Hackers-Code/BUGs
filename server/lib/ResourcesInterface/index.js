11476c5f1a70a32f1686068d7cd13d945193fb48
BOAKGP <mmm2000c4@gmail.com>
2017-04-17 16:15:10 +0200

    Renamed property

--- server/lib/ResourcesInterface/index.js
+++ server/lib/ResourcesInterface/index.js
@@ -4,10 +4,10 @@ const ResourcesDownloader = require( './ResourcesDownloader' );
 const Weapons = require( './Weapons' );
 const MapsAPI = require( './MapsAPI' );
 class ResourcesInterface {
-	constructor( projectPath )
+	constructor( projectDirectory )
 	{
-		this.resourcesPath = projectPath + '/resources';
-		this.resourcesDownloader = new ResourcesDownloader( this.resourcesPath );
+		this.resourcesDirectory = projectDirectory + '/resources';
+		this.resourcesDownloader = new ResourcesDownloader( this.resourcesDirectory );
 	}
 
 	download( callback )
@@ -17,7 +17,7 @@ class ResourcesInterface {
 
 	loadWeapons( callback )
 	{
-		this.loadResourcesListJSON( this.resourcesPath + '/weapons', ( error, array ) =>
+		this.loadResourcesListJSON( this.resourcesDirectory + '/weapons', ( error, array ) =>
 		{
 			if( error )
 			{
@@ -32,7 +32,7 @@ class ResourcesInterface {
 
 	loadMapsAPI( callback )
 	{
-		this.loadResourcesListJSON( this.resourcesPath + '/maps', ( error, array ) =>
+		this.loadResourcesListJSON( this.resourcesDirectory + '/maps', ( error, array ) =>
 		{
 			if( error )
 			{
@@ -40,7 +40,7 @@ class ResourcesInterface {
 			}
 			else
 			{
-				callback( void 0, new MapsAPI( array, this.resourcesPath ) )
+				callback( void 0, new MapsAPI( array, this.resourcesDirectory ) )
 			}
 		} );
 	}
