24a57a53d24da6cb3d2b8acb720f745502ebff3f
BOAKGP <mmm2000c4@gmail.com>
2017-04-17 16:56:06 +0200

    MapParser is now part of MapAPI

--- server/lib/ResourcesInterface/MapsAPI/index.js
+++ server/lib/ResourcesInterface/MapsAPI/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const fs = require( 'fs' );
-const MapParser = require( './MapParser' );
+const MapFormat = require( './MapFormat' );
 const Collection = require( '../../Collections' ).NumericIdCollection;
 class MapsAPI extends Collection {
 	constructor( array, resourcesPath )
@@ -33,7 +33,7 @@ class MapsAPI extends Collection {
 			{
 				try
 				{
-					let parsedMap = MapParser.parse( data );
+					let parsedMap = this.parse( data );
 					callback( void 0, parsedMap );
 				}
 				catch( parseMapError )
@@ -47,6 +47,37 @@ class MapsAPI extends Collection {
 	mapExists( id )
 	{
 		return this.find( id ) !== -1;
+	}
+
+	parse( map )
+	{
+		let mapStruct = {};
+		let offset = 0;
+		while( offset < map.length )
+		{
+			let opcode = map[ offset++ ];
+			let object = {};
+			let sectionData = MapFormat[ opcode ];
+			if( typeof sectionData !== 'object' )
+			{
+				throw new Error( 'Map file broken' );
+			}
+			let sectionName = sectionData.name;
+			if( typeof mapStruct[ sectionName ] === 'undefined' )
+			{
+				mapStruct[ sectionData.name ] = [];
+			}
+			sectionData.values.forEach( ( element ) =>
+			{
+				if( offset + element.length > map.length )
+				{
+					throw new Error( 'Unexpected end of file' );
+				}
+				object[ element.name ] = map.slice( offset, offset += element.length );
+			} );
+			mapStruct[ sectionName ].push( object );
+		}
+		return mapStruct;
 	}
 }
 module.exports = MapsAPI;
