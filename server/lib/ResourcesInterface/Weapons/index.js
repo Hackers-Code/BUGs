8105342a5b9e3dd9ed9283203979afe9db410480
BOAKGP <mmm2000c4@gmail.com>
2017-05-02 19:27:12 +0200

    Fixed issue that weapon list reference was returned instead of copy

--- server/lib/ResourcesInterface/Weapons/index.js
+++ server/lib/ResourcesInterface/Weapons/index.js
@@ -2,7 +2,21 @@
 class Weapons {
 	getWeaponsList()
 	{
-		return this._weaponsList.slice();
+		let weaponsList = [];
+		for( let i = 0 ; i < this._weaponsList.length ; i++ )
+		{
+			weaponsList.push( {
+				id : this._weaponsList[ i ].id,
+				usages : this._weaponsList[ i ].usages,
+				dmg : this._weaponsList[ i ].dmg || 0,
+				radius : this._weaponsList[ i ].radius || 0,
+				notEndRound : this._weaponsList[ i ].radius || false,
+				explosive : this._weaponsList[ i ].explosive || false,
+				delay : this._weaponsList[ i ].delay || 0,
+				bounces : this._weaponsList[ i ].bounces || false
+			} );
+		}
+		return weaponsList;
 	}
 
 	constructor( array )
